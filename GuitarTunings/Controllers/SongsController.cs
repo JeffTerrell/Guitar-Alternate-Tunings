4b9d14b9df5f71b0c1310bfa57f12859cfd54399
Jeff Terrell <jeffaterrell@gmail.com>
2022-03-08 19:06:51 -0500

    add song controller and views with CRUD

---
+++ GuitarTunings/Controllers/SongsController.cs
@@ -0,0 +1,77 @@
+using System.Linq;
+using GuitarTunings.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuitarTunings.Controllers
+{
+
+  public class SongsController : Controller
+  {
+    private readonly GuitarTuningsContext _db;
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public SongsController(GuitarTuningsContext db, IWebHostEnvironment hostEnvironment)
+    {
+      _db = db;
+      this._hostEnvironment = hostEnvironment;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Songs.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.TuningId = new SelectList(_db.Tunings, "TuningId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create (Song song)
+    {
+      _db.Songs.Add(song);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int Id)
+    {
+      Song thisSong = _db.Songs.FirstOrDefault(song => song.SongId == Id);
+      return View(thisSong);
+    }
+
+    public ActionResult Edit(int Id)
+    {
+      ViewBag.TuningId = new SelectList(_db.Tunings, "TuningId", "Name");
+      Song thisSong = _db.Songs.FirstOrDefault(song => song.SongId == Id);
+      return View(thisSong);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Song song)
+    {
+      _db.Entry(song).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = song.SongId});
+    }
+
+    public ActionResult Delete(int Id)
+    {
+      Song thisSong = _db.Songs.FirstOrDefault(song => song.SongId == Id);
+      return View(thisSong);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int Id)
+    {
+      Song thisSong = _db.Songs.FirstOrDefault(song => song.SongId == Id);
+      _db.Songs.Remove(thisSong);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
