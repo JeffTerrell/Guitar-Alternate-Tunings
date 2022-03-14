ab7d81da7c4abbdf4cee57d5a278aafd62f743bb
Jeff Terrell <jeffaterrell@gmail.com>
2022-03-13 22:16:34 -0400

    hide password input from login screen

--- GuitarTunings/ViewModels/LoginViewModel.cs
+++ GuitarTunings/ViewModels/LoginViewModel.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GuitarTunings.ViewModels
 {
   public class LoginViewModel
   {
+    [Required]
     public string UserName {get; set;}
+    [Required]
+    [DataType(DataType.Password)]
     public string Password {get; set;}
   }
 }
