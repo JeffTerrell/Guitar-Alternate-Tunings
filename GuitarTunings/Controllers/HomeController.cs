153a797fa14bcf4bcf238c4da81ac6d10c347412
Jeff Terrell <jeffaterrell@gmail.com>
2022-07-15 21:55:40 -0400

    add latest songs added to home index.cshtml

--- GuitarTunings/Controllers/HomeController.cs
+++ GuitarTunings/Controllers/HomeController.cs
@@ -1,15 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GuitarTunings.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GuitarTunings.Controllers
 {
 
+  [Authorize]
   public class HomeController : Controller
   {
+    private readonly GuitarTuningsContext _db;
 
+    public HomeController(GuitarTuningsContext db)
+    {
+      _db = db;
+    }
+
+    [AllowAnonymous]
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      return View(_db.Songs.OrderByDescending(song => song.SongId).Take(5).ToList());
     }
   }
 }
+
+
+
+// var songTest = _db.Songs.OrderByDescending(song => song.Name).Take(1).ToString();
+// var songTest2 = _db.Songs.FirstOrDefault(song => song.Name == "teatsdf");
+// ViewBag.LastUpdated = "Last Updated: " + System.IO.File.GetLastWriteTime(songTest2.Name);
+// ViewBag.aSong = songTest2.Name;
+// ViewBag.LastUpdated = "Last Updated: " + System.IO.File.GetLastWriteTime(_db.Songs.ToString());
